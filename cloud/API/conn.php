f5296c4b49ef74a914136a59f9cb536beef2359c
nuriansyahPutra <46669729+Riansyah5@users.noreply.github.com>
2022-05-28 22:03:46 +0700

    Update conn.php

--- cloud/API/conn.php
+++ cloud/API/conn.php
@@ -1,9 +1,9 @@
 <?php 
 
 	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "db_sensor";
+	$username = "id18791339_root";
+	$password = "k\EOX&&v6lAt@FGA";
+	$dbname = "id18791339_db_sensor";
 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
